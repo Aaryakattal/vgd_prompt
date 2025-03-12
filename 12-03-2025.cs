819bcb3f2725341696bd450547ed1ff011493869
Aaryakattal <aaryakattal223@gmail.com>
2025-03-12 08:43:43 -0600

    Create 12-03-2025.cs

---
+++ 12-03-2025.cs
@@ -0,0 +1,10 @@
+// Today i learned how to Combine strings using string concatenation for simply combining two or more string values into a new string value.
+eg. 
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+
+//The in unit 4 i learned Combine strings using string interpolation 
+eg.
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
