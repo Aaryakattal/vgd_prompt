8c6f8965d88d78d66ecaaf732e316bebfdbc71c5
Aaryakattal <aaryakattal223@gmail.com>
2025-03-12 08:49:40 -0600

    Update 12-03-2025.cs

--- 12-03-2025.cs
+++ 12-03-2025.cs
@@ -1,12 +1,16 @@
 // Today i learned how to Combine strings using string concatenation for simply combining two or more string values into a new string value.
 eg. 
+  
 string firstName = "Bob";
 string greeting = "Hello";
 Console.WriteLine(greeting + " " + firstName + "!");
 
-//The in unit 4 i learned Combine strings using string interpolation 
+//The in unit 4 i learned Combine strings using string interpolation, String interpolation combines multiple values into a single literal string by using a "template" and one or more interpolation expressions.
+You can combine string interpolation and verbatim literals by combining the symbols for each and using that as a prefix for the string template.
+  
 eg.
+  
 string projectName = "First-Project";
 Console.WriteLine($@"C:\Output\{projectName}\Data");
 
-THis is using a verbatium string literal 
+This is using a verbatium string literal which i told in the last journal response 
