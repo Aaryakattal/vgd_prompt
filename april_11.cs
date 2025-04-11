c59a1c6855c90300fa737f6855d51306573711cb
Aaryakattal <aaryakattal223@gmail.com>
2025-04-11 10:12:30 -0600

    Update april_11.cs

--- april_11.cs
+++ april_11.cs
@@ -1,1 +1,7 @@
+// Apil 11th: Today i learned how to convert celsius to fahrenheight on a scale using operations in C#
+code: 
+int fahrenheit = 94;
+decimal celsius = (fahrenheit-32) * (5m/9m);
+Console.WriteLine($"the temprature is"+ celsius +" Celsius");
+
 
